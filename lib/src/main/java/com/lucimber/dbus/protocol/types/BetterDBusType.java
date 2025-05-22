5fc0c23169557033fba4a435c354f9a3ba4817b2
Sebastian Veit <sebastian.veit@lucimber.com>
2025-05-22 18:56:55 +0200

    Revert "protocol: Visitor for (de)serialization"
    This reverts commit 491503e8ed137e3cb70734eb4393dd92b5c04ce0.
    
    Signed-off-by: Sebastian Veit <sebastian.veit@lucimber.com>

--- lib/src/main/java/com/lucimber/dbus/protocol/types/BetterDBusType.java
+++ lib/src/main/java/com/lucimber/dbus/protocol/types/BetterDBusType.java
@@ -5,8 +5,6 @@
 
 package com.lucimber.dbus.protocol.types;
 
-import com.lucimber.dbus.protocol.visitor.DBusTypeVisitor;
-
 /**
  * Sealed root for all D-Bus runtime types.
  */
@@ -46,33 +44,4 @@ public sealed interface BetterDBusType
    * @return the Java representation (e.g. Integer, String, List, Map)
    */
   Object getValue();
-
-  /**
-   * Accepts a {@link DBusTypeVisitor} to perform operations based on
-   * the concrete D-Bus type instance.
-   *
-   * @param visitor the visitor to dispatch to
-   * @param <R> the return type of the visitor methods
-   * @return the result of visiting this type
-   */
-  default <R> R accept(final DBusTypeVisitor<R> visitor) {
-    if (this instanceof BetterDBusByte b)      return visitor.visitByte(b);
-    if (this instanceof BetterDBusBoolean b)   return visitor.visitBoolean(b);
-    if (this instanceof BetterDBusInt16 b)     return visitor.visitInt16(b);
-    if (this instanceof BetterDBusUInt16 b)    return visitor.visitUInt16(b);
-    if (this instanceof BetterDBusInt32 b)     return visitor.visitInt32(b);
-    if (this instanceof BetterDBusUInt32 b)    return visitor.visitUInt32(b);
-    if (this instanceof BetterDBusInt64 b)     return visitor.visitInt64(b);
-    if (this instanceof BetterDBusUInt64 b)    return visitor.visitUInt64(b);
-    if (this instanceof BetterDBusDouble b)    return visitor.visitDouble(b);
-    if (this instanceof BetterDBusString b)    return visitor.visitString(b);
-    if (this instanceof BetterDBusObjectPath b) return visitor.visitObjectPath(b);
-    if (this instanceof BetterDBusSignature b) return visitor.visitSignature(b);
-    if (this instanceof BetterDBusUnixFd b)    return visitor.visitUnixFd(b);
-    if (this instanceof BetterDBusArray b)     return visitor.visitArray(b);
-    if (this instanceof BetterDBusStruct b)    return visitor.visitStruct(b);
-    if (this instanceof BetterDBusDictEntry b) return visitor.visitDictEntry(b);
-    if (this instanceof BetterDBusVariant b)   return visitor.visitVariant(b);
-    throw new IllegalStateException("Unknown DBusType: " + this.getClass());
-  }
 }
